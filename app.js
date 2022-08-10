8ff27a62e162c0b1435c18275eaa9181af595ebd
l-ohman <jakelohman7@gmail.com>
2022-08-10 15:29:42 -0500

    Full commit - routes

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const index = require("./views/index");
+const { db, Page, User } = require("./models");
+
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+const usersRouter = require("./routes/users");
+const wikiRouter = require("./routes/wiki");
+app.use('/users', usersRouter);
+app.use('/wiki', wikiRouter);
+
+db.authenticate()
+    .then(() => {
+        console.log('connected to the database');
+    })
 
 app.get("/", (req, res) => {
     res.send(index.main());
 })
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-})
+const init = async () => {
+    await db.sync({force: true});
+
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    })
+}
+init();
