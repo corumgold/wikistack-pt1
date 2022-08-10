e605cab0c71b147ba11764d282bd9aae6db7b390
Corum Gold <goldcorum@gmail.com>
2022-08-10 16:33:24 -0400

    Connect add page route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const viewsIndex = require("../views/index");
 
 router.get("/", (req, res, next) => {
-    try {
-        res.send('wiki page');
-    }
-    catch (err) { next(err) }
+  try {
+    res.send("wiki page");
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/add", (req, res, next) => {
-    try {
-        res.send('add page');
-    }
-    catch (err) { next(err) }
+  try {
+    res.send(viewsIndex.addPage());
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/", (req, res, next) => {
-    try {
-        res.send('Got to post!');
-    }
-    catch (err) { next(err) }
-})
+  try {
+    res.send("Got to post!");
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
